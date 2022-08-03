dcec778052ac77f2c609c97ab73eedf0357b0107
Emre <E22536@eon.com>
2022-08-03 11:30:32 +0200

    ...

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,19 @@
 import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function firstPost() {
   return (
     <div>
+      <Head>
+        <title>First post</title>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnLoad"
+          onLoad={() => console.log(`script loaded correctly`)}
+        />
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
